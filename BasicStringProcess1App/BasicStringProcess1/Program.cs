851a333f03e15e495c1e5d21984076de0773f9bb
ashwathgowdanp <ashwathgowdanp@gmail.com>
2023-02-06 08:13:18 -0500

    Basic string processing commit

---
+++ BasicStringProcess1App/BasicStringProcess1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Basic String Processing
+
+using System.Collections.Generic;
+List<string> UserSentence = new List<string>();
+
+do
+{
+    Console.WriteLine( "Please enter a sentence below: " );
+    var Value = Console.ReadLine();
+    if (Value != null)
+    {
+        UserSentence = Value.Split(" ").ToList();
+        foreach (string n in UserSentence)
+        {
+            Console.WriteLine(n);
+        }
+    }
+    else
+        break;
+    Console.WriteLine("Would you like to continue: (y/n)? ");
+    var UserAnswer = Console.ReadLine();
+    if (UserAnswer != null)
+    {
+        if (UserAnswer.ToLower() == "n")
+        {
+            break;
+        }
+    }
+    else
+        break;
+
+} while(true);
---
+++ BasicStringProcessing2App/BasicStringProcessing2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Repeatdly prompt user for entering the string and display
+List<string> usersentence= new List<string>();
+
+do
+{
+    Console.Write("Enter some text: ");
+    var userstring = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(userstring) && (!string.IsNullOrEmpty(userstring)))
+    {
+        usersentence.Add(userstring);
+        Console.Write("You have entered: ");
+        foreach (string n in usersentence)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+        Console.Write("Would you like to continue: (y/n)? ");
+        var UserAnswer = Console.ReadLine();
+        if (!string.IsNullOrEmpty(UserAnswer))
+        {
+            if (UserAnswer.ToLower() == "n")
+            {
+                Console.WriteLine("GoodBye!");
+                break;
+            }
+            else
+                continue;
+        }
+        else
+            break;
+
+    }
+    else
+        break;
+       
+
+    
+        
+}while(true);
